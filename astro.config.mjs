f7908e45a0580f42e10e2b92d7a6218e04e35157
muhsin <muhsin7ak@gmail.com>
2026-09-11 21:49:38 +0530

    fix: base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://muhsin7majeed.github.io',
-  base: process.env.BASE_PATH || '/realms',
+  base: process.env.BASE_PATH || '/realms/',
   output: 'static',
 });
